c3ba448e2a631b4f1e94e34664649622e510775b
vndna-rejhra <wandhnarejhra@gmail.com>
2024-07-17 19:53:27 +0500

    Add files via upload
    Calculator

---
+++ scripit.js
@@ -0,0 +1,25 @@
+let input =document.getElementById('input-box');
+let buttons =document.querySelectorAll('button');
+let String="";
+let arr=Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+            String=eval(String);
+            input.value=String;
+        }
+else if(e.target.innerHTML=='AC'){
+    String="";
+    input.value=String;
+}
+else if(e.target.innerHTML=='DEL'){
+    String=String.SubString(0,String.length-1);
+    input.value=String;
+}
+else{
+    String += e.target.innerHTML;
+    input.value=String;
+}
+       
+    })
+})
